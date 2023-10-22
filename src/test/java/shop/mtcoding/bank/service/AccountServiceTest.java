72ef369cb529bd953d7e53537cffe6d964a146f7
sunghyun1999 <32184099@dankook.ac.kr>
2023-10-22 17:20:38 +0900

    계좌등록 테스트코드 작성

--- src/test/java/shop/mtcoding/bank/service/AccountServiceTest.java
+++ src/test/java/shop/mtcoding/bank/service/AccountServiceTest.java
@@ -1,10 +1,64 @@
 package shop.mtcoding.bank.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import shop.mtcoding.bank.config.dummy.DummyObject;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.account.AccountRepository;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserRepository;
+import shop.mtcoding.bank.dto.account.AccountReqDto;
+import shop.mtcoding.bank.dto.account.AccountRespDto;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class AccountServiceTest {
+public class AccountServiceTest extends DummyObject {
 
+    @InjectMocks // 모든 Mock들이 InjectMocks로 주입됨
+    private AccountService accountService;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private AccountRepository accountRepository;
+    @Spy
+    private ObjectMapper om;
 
+    @Test
+    public void 계좌등록_test() throws Exception {
+        // given
+        Long userId = 1L;
+
+        AccountReqDto.AccountSaveReqDto accountSaveReqDto = new AccountReqDto.AccountSaveReqDto();
+        accountSaveReqDto.setNumber(1111L);
+        accountSaveReqDto.setPassword(1234L);
+
+        // stub 1
+        User jsh = newMockUser(userId, "jsh", "정성현");
+        when(userRepository.findById(any())).thenReturn(Optional.of(jsh));
+
+        // stub 2
+        when(accountRepository.findByNumber(any())).thenReturn(Optional.empty());
+
+        // stub 3
+        Account jshAccount = newMockAccount(1L, 1111L, 1000L,  jsh);
+        when(accountRepository.save(any())).thenReturn(jshAccount);
+
+        // when
+        AccountRespDto.AccountSaveRespDto accountSaveRespDto = accountService.계좌등록(accountSaveReqDto, userId);
+        String responseBody = om.writeValueAsString(accountSaveRespDto);
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        assertThat(accountSaveRespDto.getNumber()).isEqualTo(1111L);
+    }
 }
